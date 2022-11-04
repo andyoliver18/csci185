778b3b44e874a900cd9b01d36ed5d9b92f1187e4
Andy <andyuhuru@gmail.com>
2022-11-04 14:05:17 -0400

    links

---
+++ tutorials/tutorial12/exercise01/main.js
@@ -0,0 +1,21 @@
+function makeRed() {
+    // your code here...
+    document.querySelector("section").style.backgroundColor='red';
+    console.log('Change the box\'s background color to red');
+}
+
+function makeBlue() {
+    document.querySelector("section").style.backgroundColor='blue';
+    console.log('Change the box\'s background color to blue');
+}
+
+function makePink() {
+    document.querySelector("section").style.backgroundColor='pink';
+    console.log('Change the box\'s background color to pink');
+}
+
+function makeOrange() {
+    document.querySelector("section").style.backgroundColor='orange';
+    console.log('Change the box\'s background color to orange');
+}
+
